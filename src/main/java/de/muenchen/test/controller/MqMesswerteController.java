a9ebaa403988477eca5df1d9f8dec8665e267e5c
martin.dietrich <martin.dietrich@muenchen.de>
2024-10-08 15:23:33 +0200

    license headers added

--- src/main/java/de/muenchen/test/controller/MqMesswerteController.java
+++ src/main/java/de/muenchen/test/controller/MqMesswerteController.java
@@ -1,3 +1,25 @@
+/*
+ * The MIT License
+ * Copyright © 2023 Landeshauptstadt München | it@M
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package de.muenchen.test.controller;
 
 import de.muenchen.test.domain.Constants;
@@ -33,7 +55,7 @@ public class MqMesswerteController {
 
     private final MesswerteService service;
 
-    @PreAuthorize("hasRole(T(de.muenchen.test.domain.Constants).CLIENT_ROLE.name())")
+    @PreAuthorize("hasRole(T(de.muenchen.test.domain.Constants).CLIENT_ROLE)")
     @GetMapping(value = "/year", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MqMesswerteDTO> loadMesswerteByYear(@RequestParam(name = "year") Integer year) {
         log.info("loadMesswerteByYear");
@@ -64,7 +86,7 @@ public class MqMesswerteController {
                             content = @Content
                     ) }
     )
-    @PreAuthorize("hasRole(T(de.muenchen.test.domain.Constants).CLIENT_ROLE.name())")
+    @PreAuthorize("hasRole(T(de.muenchen.test.domain.Constants).CLIENT_ROLE)")
     @GetMapping(value = "/fullrange", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MqMesswerteDTO> loadMesswerteFullRange(@RequestParam(name = "messquerschnitte") List<String> messquerschnitte,
             @RequestParam(name = "datumVon") LocalDateTime datumVon,
--- src/main/java/de/muenchen/test/domain/Tagestyp.java
+++ src/main/java/de/muenchen/test/domain/Tagestyp.java
@@ -1,9 +1,32 @@
+/*
+ * The MIT License
+ * Copyright © 2023 Landeshauptstadt München | it@M
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package de.muenchen.test.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public enum Tagestyp {
     WERKTAG_DI_MI_DO(1), WERKTAG_MO_FR(2), SAMSTAG(
             3), SONNTAG_FEIERTAG(4), WERKTAG_FERIEN(5);
@@ -11,7 +34,6 @@ public enum Tagestyp {
     /**
      * Die Id zum TagesTyp.
      */
-    @Getter
     private final Integer id;
 
 }
