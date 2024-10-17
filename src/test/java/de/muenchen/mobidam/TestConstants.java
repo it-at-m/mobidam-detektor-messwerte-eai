aa66cd1294d1926e042d8c2f1ed275448650836c
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-10-17 14:56:07 +0200

    add unit test

---
+++ src/test/java/de/muenchen/mobidam/TestConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2020
+ */
+package de.muenchen.mobidam;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class TestConstants {
+
+    public static final String SPRING_TEST_PROFILE = "unittest";
+
+    public static final String SPRING_NO_SECURITY_PROFILE = "no-security";
+
+}
--- src/test/java/de/muenchen/mobidam/repository/MqMesswerteRepositoryTest.java
+++ src/test/java/de/muenchen/mobidam/repository/MqMesswerteRepositoryTest.java
@@ -1,7 +1,25 @@
 package de.muenchen.mobidam.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import de.muenchen.mobidam.MicroServiceApplication;
+import de.muenchen.mobidam.TestConstants;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest(
+        classes = { MicroServiceApplication.class },
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@ActiveProfiles(profiles = { TestConstants.SPRING_TEST_PROFILE, TestConstants.SPRING_NO_SECURITY_PROFILE })
 class MqMesswerteRepositoryTest {
+
+    @Autowired
+    private MqMesswerteRepository mqMesswerteRepository;
+
+    @Test
+    void test() {
+
+    }
 
 }
